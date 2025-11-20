87cba389cbf4c1acf697f266a2536cd96f9b080b
Yongchun Li <yongchunl@student.unimelb.edu.au>
2025-11-20 20:06:03 +0800

    BE init

---
+++ tdl/src/main/java/cn/bitsleep/tdl/TdlApplication.java
@@ -0,0 +1,13 @@
+package cn.bitsleep.tdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdlApplication.class, args);
+    }
+
+}
---
+++ tdl/src/test/java/cn/bitsleep/tdl/TdlApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bitsleep.tdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
